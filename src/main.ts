11c87c0c1da24453a67c9d6f8e2ae9799c04aa64
Carson <carson.sturtevant@gmail.com>
2025-06-03 23:04:28 -0600

    with service worker

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+if (navigator.serviceWorker) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('ngsw-worker.js');
+  });
+}
+
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
